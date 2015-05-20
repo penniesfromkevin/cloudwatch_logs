b16e11dbad338d1b60d5da31fd964332fe61a4ee
kevin <kevin@wavefront.com>
2015-05-19 18:13:28 -0700

    Added awslogs-agent-setup script as a templated item for configurable logs.

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,11 +5,13 @@ default['cloudwatch_logs']['log_groups'] = [
     'file' => '/var/log/awslogs.log'
   }
 ]
+# If this is empty, the template version of the setup file will be used.
+default['cloudwatch_logs'][:aws_agent_url] = 'https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/awslogs-agent-setup.py'
 
 # Is there such a thing as over-parameterization?
+default['cloudwatch_logs']['log_file'] = '/var/log/awslogs.log'
 default['cloudwatch_logs'][:cfg_file] = '/tmp/cloudwatch_logs.cfg'
 default['cloudwatch_logs'][:agent_dir] = '/opt/aws/cloudwatch_logs'
-default['cloudwatch_logs'][:aws_agent_url] = 'https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/awslogs-agent-setup.py'
 default['cloudwatch_logs']['log_group_defaults'] = {
   'datetime_format' => '%d/%b/%Y:%H:%M:%S',
   'buffer_duration' => 5000,
--- recipes/install.rb
+++ recipes/install.rb
@@ -2,9 +2,18 @@ directory node['cloudwatch_logs']['agent_dir'] do
   recursive true
 end
 
-remote_file "#{node['cloudwatch_logs']['agent_dir']}/awslogs-agent-setup.py" do
-  source node['cloudwatch_logs']['aws_agent_url']
-  mode "0755"
+if node['cloudwatch_logs']['aws_agent_url']
+  remote_file "#{node['cloudwatch_logs']['agent_dir']}/awslogs-agent-setup.py" do
+    source node['cloudwatch_logs']['aws_agent_url']
+    mode '0755'
+  end
+else
+  template "#{node['cloudwatch_logs']['agent_dir']}/awslogs-agent-setup.py" do
+    source 'awslogs-agent-setup.py.erb'
+    owner 'root'
+    group 'root'
+    mode '0755'
+  end
 end
 
 execute "Install CloudWatch Logs agent" do
